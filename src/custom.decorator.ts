9914d0c8053b552e4711a080252fdf404071e722
beita <beita0527@163.com>
2024-01-01 16:04:05 +0800

    feat: 封装自定义装饰器UserInfo用来获取user信息传入handler

--- src/custom.decorator.ts
+++ src/custom.decorator.ts
@@ -1,5 +1,20 @@
-import { SetMetadata } from '@nestjs/common';
+import {
+  ExecutionContext,
+  SetMetadata,
+  createParamDecorator,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 export const RequireLogin = () => SetMetadata('require-login', true);
 export const RequirePermission = (...permissions: string[]) =>
   SetMetadata('require-permission', permissions);
+// todo  UserInfo 装饰器是用来取 user 信息传入 handler 的。
+export const UserInfo = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    if (!request.user) {
+      return null;
+    }
+    return data ? request.user[data] : request.user;
+  },
+);
