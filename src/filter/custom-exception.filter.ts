96a37d456a3aefb86b2abbf997c2ff32ceed73b8
beita <beita0527@163.com>
2024-01-02 23:17:06 +0800

    fix: customExxception 获取ValidationPipe报错信息异常

--- src/filter/custom-exception.filter.ts
+++ src/filter/custom-exception.filter.ts
@@ -11,11 +11,13 @@ export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
     response.statusCode = exception.getStatus();
+    // todo 获取 ValidationPipe 的报错信息
+    const res = exception.getResponse() as { message: string[] };
     response
       .json({
         code: exception.getStatus(),
         message: 'fail😭',
-        data: exception.message,
+        data: res?.message?.join(',') || exception.message,
       })
       .end();
   }
